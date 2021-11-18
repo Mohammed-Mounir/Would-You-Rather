a27c9fb14c5bcd5c37f266883b4a662cdb7f7bc0
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-11-18 15:35:31 +0200

    feat: Add question action and reducer

--- src/state/action-creator/questions.js
+++ src/state/action-creator/questions.js
@@ -1,4 +1,27 @@
+import { saveQuestion } from "../../utils/api";
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const ADD_QUESTION = "ADD_QUESTION";
+
+export const addQuestion = (question) => {
+  return (dispatch) => {
+    dispatch({
+      type: ADD_QUESTION,
+      question,
+    });
+  };
+};
+
+export function handleAddQuestion(optionOneText, optionTwoText) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    return saveQuestion({
+      optionOneText,
+      optionTwoText,
+      author: authedUser,
+    }).then((question) => dispatch(addQuestion(question)));
+  };
+}
 
 export const receiveQuestions = (questions) => {
   return {
--- src/state/reducers/questionsReducer.js
+++ src/state/reducers/questionsReducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS } from "../action-creator/questions";
+import { RECEIVE_QUESTIONS, ADD_QUESTION } from "../action-creator/questions";
 
 const reducer = (state = null, action) => {
   switch (action.type) {
@@ -6,6 +6,12 @@ const reducer = (state = null, action) => {
       return {
         ...state,
         ...action.questions,
+      };
+
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.id]: action.question,
       };
 
     default:
