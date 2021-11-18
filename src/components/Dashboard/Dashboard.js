dc61692d211f065d5dd44b880f4caa353134c9c0
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-11-18 10:48:33 +0200

    feat: Basic tabs

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,27 @@
 import QuestionView from "../QuestionCards/QuestionView";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleChange = (evt, newValue) => {
+    setSelectedTab(newValue);
+  };
   return (
     <div>
-      <div>
-        <h3>Unanswered Questions 2</h3>
-        <QuestionView />
-      </div>
-      <div>
-        <h3>Answered Questions 5</h3>
-        <QuestionView />
-      </div>
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={selectedTab} onChange={handleChange}>
+            <Tab label="Unanswered Questions" />
+            <Tab label="Answered Questions" />
+          </Tabs>
+        </Box>
+        {selectedTab === 0 && <p>I'm number 0</p>}
+        {selectedTab === 1 && <p>I'm number 1</p>}
+      </Box>
     </div>
   );
 };
