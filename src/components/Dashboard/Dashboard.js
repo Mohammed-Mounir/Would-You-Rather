3fb1c4e43f7e450b467f60d1b23683e939277150
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-11-18 13:01:05 +0200

    feat: Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,26 +3,64 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 const Dashboard = () => {
+  const { users, authedUser, questions } = useSelector((state) => state);
   const [selectedTab, setSelectedTab] = useState(0);
+
+  const currentUser = users[authedUser];
+  const currentUserAnswered = currentUser?.answers;
+
+  const unAnsweredQuestions =
+    currentUserAnswered &&
+    Object.entries(questions).filter(
+      ([_, { id: questionID }]) =>
+        !Object.keys(currentUserAnswered).some(
+          (answeredQuestionID) => answeredQuestionID === questionID
+        )
+    );
+
+  const answeredQuestions =
+    currentUserAnswered &&
+    Object.entries(questions).filter(([_, { id: questionID }]) =>
+      Object.keys(currentUserAnswered).some(
+        (answeredQuestionID) => answeredQuestionID === questionID
+      )
+    );
 
   const handleChange = (evt, newValue) => {
     setSelectedTab(newValue);
   };
   return (
-    <div>
-      <Box sx={{ width: "100%" }}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs value={selectedTab} onChange={handleChange}>
-            <Tab label="Unanswered Questions" />
-            <Tab label="Answered Questions" />
-          </Tabs>
-        </Box>
-        {selectedTab === 0 && <p>I'm number 0</p>}
-        {selectedTab === 1 && <p>I'm number 1</p>}
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={selectedTab} onChange={handleChange}>
+          <Tab label="Unanswered Questions" />
+          <Tab label="Answered Questions" />
+        </Tabs>
       </Box>
-    </div>
+
+      <Container>
+        <Grid container spacing={3} justifyContent="center">
+          {selectedTab === 0 &&
+            unAnsweredQuestions?.map((question) => (
+              <Grid item xs={8} key={question[0]}>
+                <QuestionView question={question[1]} />
+              </Grid>
+            ))}
+
+          {selectedTab === 1 &&
+            answeredQuestions?.map((question) => (
+              <Grid item xs={8} key={question[0]}>
+                <QuestionView question={question[1]} />
+              </Grid>
+            ))}
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
--- src/components/QuestionCards/QuestionView.js
+++ src/components/QuestionCards/QuestionView.js
@@ -1,5 +1,54 @@
-const QuestionView = () => {
-  return <div>QuestionView</div>;
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import { red, blue } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+
+const CardText = styled("p")(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: blue[500],
+  textAlign: "center",
+  padding: theme.spacing(1),
+  color: "white",
+}));
+
+const QuestionView = (props) => {
+  const users = useSelector((state) => state.users);
+
+  const { id, author, timestamp, optionOne, optionTwo } = props.question;
+  const questionUserName = users[author]?.name;
+  const questionUserImage = users[author]?.avatarURL;
+
+  return (
+    <Card elevation={1} sx={{ marginTop: "20px" }}>
+      <CardHeader
+        avatar={
+          <Avatar
+            alt={`Photo of ${questionUserName}`}
+            src={questionUserImage}
+          />
+        }
+        title={
+          <Typography variant="h6" sx={{ color: "#808080" }}>
+            {`${questionUserName} asks would you rather..`}
+          </Typography>
+        }
+      />
+      <CardContent sx={{ textAlign: "center" }}>
+        <CardText>{optionOne.text}?</CardText>
+        <CardText sx={{ bgcolor: "white", color: "#808080" }}>OR</CardText>
+        <CardText>{optionTwo.text}?</CardText>
+        <Button variant="outlined" sx={{ width: "100%", marginTop: "10px" }}>
+          View Question
+        </Button>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default QuestionView;
